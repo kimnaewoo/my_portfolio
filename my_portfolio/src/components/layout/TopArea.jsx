778b5b6cb2e6d5f819495340627925d1a44cb13d
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2024-01-24 22:07:33 +0900

    포트포리오 CSS 및 레이아웃 수정

--- my_portfolio/src/components/layout/TopArea.jsx
+++ my_portfolio/src/components/layout/TopArea.jsx
@@ -1,51 +1,51 @@
-import { useEffect } from "react";
-import { Aboutme } from "../pages/Aboutme";
-import { Project } from "../pages/Project";
+import { useEffect } from 'react';
+import { Aboutme } from '../pages/Aboutme';
+import { Project } from '../pages/Project';
 
 export function TopArea() {
   const showPJ = () => {
-    let about = document.querySelector("#about");
-    let pj = document.querySelector("#projects");
-    let skl = document.querySelector(".skill");
-    let skls = document.querySelector(".skill span");
-    let itd = document.querySelector(".introduce");
-    let itds = document.querySelector(".introduce span");
-    about.style.display = "none";
-    pj.style.display = "flex";
-    skl.style.backgroundColor = "#fff";
-    skls.style.color = "rgb(0, 71, 187)";
-    itd.style.backgroundColor = "rgb(0, 71, 187)";
-    itds.style.color = "#fff";
-    itds.style.fontSize = "1rem";
-    skls.style.fontSize = "1.4rem";
+    let about = document.querySelector('#about');
+    let pj = document.querySelector('#projects');
+    let skl = document.querySelector('.skill');
+    let skls = document.querySelector('.skill span');
+    let itd = document.querySelector('.introduce');
+    let itds = document.querySelector('.introduce span');
+    about.style.display = 'none';
+    pj.style.display = 'flex';
+    skl.style.backgroundColor = '#fff';
+    skls.style.color = 'rgb(0, 71, 187)';
+    itd.style.backgroundColor = 'rgb(0, 71, 187)';
+    itds.style.color = '#fff';
+    itds.style.fontSize = '1rem';
+    skls.style.fontSize = '1.4rem';
   };
   const showAM = () => {
-    let about = document.querySelector("#about");
-    let pj = document.querySelector("#projects");
-    let itd = document.querySelector(".introduce");
-    let itds = document.querySelector(".introduce span");
-    let skl = document.querySelector(".skill");
-    let skls = document.querySelector(".skill span");
-    about.style.display = "block";
-    pj.style.display = "none";
-    itd.style.backgroundColor = "#fff";
-    itds.style.color = "rgb(0, 71, 187)";
-    skl.style.backgroundColor = "rgb(0, 71, 187)";
-    skls.style.color = "#fff";
-    skls.style.fontSize = "1rem";
-    itds.style.fontSize = "1.4rem";
+    let about = document.querySelector('#about');
+    let pj = document.querySelector('#projects');
+    let itd = document.querySelector('.introduce');
+    let itds = document.querySelector('.introduce span');
+    let skl = document.querySelector('.skill');
+    let skls = document.querySelector('.skill span');
+    about.style.display = 'block';
+    pj.style.display = 'none';
+    itd.style.backgroundColor = '#fff';
+    itds.style.color = 'rgb(0, 71, 187)';
+    skl.style.backgroundColor = 'rgb(0, 71, 187)';
+    skls.style.color = '#fff';
+    skls.style.fontSize = '1rem';
+    itds.style.fontSize = '1.4rem';
   };
 
   useEffect(() => {
-    const homeText = document.querySelector("h1");
+    const homeText = document.querySelector('h1');
 
-    const content = "Front-end　developer";
+    const content = 'Front-end　developer';
     let count = 0;
 
     function typing() {
       homeText.innerText += content[count++];
       if (count > content.length) {
-        homeText.innerText = "";
+        homeText.innerText = '';
         count = 0;
       }
     }
--- my_portfolio/src/components/pages/Aboutme.jsx
+++ my_portfolio/src/components/pages/Aboutme.jsx
@@ -1,4 +1,4 @@
-import "../../css/aboutme.css";
+import '../../css/aboutme.css';
 
 export function Aboutme() {
   return (
@@ -20,10 +20,12 @@ export function Aboutme() {
             </div>
             <div className="introInfo">
               <p className="info">
-                2023년 코딩을 처음 접하였고 웹개발 코딩을 하기 위해 지금까지 열심히 달리고 있습니다.
+                안녕하세요. 다양한 배경색을 가진 프론트엔드 신입개발자 김내우입니다.
+                <br />
+                <br /> 2023년 코딩을 처음 접하였고 웹개발 코딩을 하기 위해 지금까지 열심히 달리고 있습니다.
                 <br /> 다소 부족하지만 부족한 지식은 하루하루 노력으로 채워가고 있습니다. <br />
-                빠르게 변하는 웹개발트랜드 속에서, 다양한 시도와 함께 사용자 입장을 고려하며 성실하게 성장하는
-                프론트엔드 개발자가 되겠습니다.
+                빠르게 변하는 웹개발트랜드 속에서,
+                <br /> 다양한 시도와 함께 사용자 입장을 고려하며 성실하게 성장하는 프론트엔드 개발자가 되겠습니다.
               </p>
             </div>
           </div>
@@ -49,7 +51,7 @@ export function Aboutme() {
             </li>
           </div>
           <div className="exp">
-            <p>✔ Etc</p>
+            <p>✔ Education</p>
             <li>
               2023. 7. 26 ~ 2024. 01. 19 <br />
               프론트엔드 웹&앱 SW개발자 과정수료 (그린컴퓨터 아카데미)
@@ -57,6 +59,10 @@ export function Aboutme() {
             <li>
               2019. 4. 10 ~ 2020. 04. 05 <br />
               워킹 & 홀리데이 (호주 시드니)
+            </li>
+            <li>
+              2011. 3. 00 ~ 2013. 02. 00 <br />
+              세종고등학교
             </li>
           </div>
         </div>
